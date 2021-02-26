315c88f521e4ac549c1a2646c8cd6fa2114402a2
t-kubrak <taras.m.kubrak@gmail.com>
2021-02-26 18:03:53 +0200

    refactor: import actions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,7 @@
 import { _getQuestions, _getUsers } from '../utils/_DATA'
-import { receiveUsers } from '../actions/users'
-import { receiveQuestions } from '../actions/questions'
-import { setAuthedUser } from '../actions/authedUser'
+import { receiveUsers } from './users'
+import { receiveQuestions } from './questions'
+import { setAuthedUser } from './authedUser'
 
 const AUTHED_ID = 'sarahedo'
 
