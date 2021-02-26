98434ca46b0550dfbfb7d36fee5008bde3f625da
t-kubrak <taras.m.kubrak@gmail.com>
2021-02-26 19:09:32 +0200

    feat: get initial data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -7,8 +7,8 @@ const AUTHED_ID = 'sarahedo'
 
 export function handleInitialData () {
   return (dispatch) => {
-    return Promise.all([_getUsers, _getQuestions()])
-      .then(({ users, questions }) => {
+    return Promise.all([_getUsers(), _getQuestions()])
+      .then(([users, questions]) => {
         dispatch(receiveUsers(users))
         dispatch(receiveQuestions(questions))
         dispatch(setAuthedUser(AUTHED_ID))
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from "../actions/shared";
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
 
-function App() {
-  return (
-    <main>
-      App
-    </main>
-  );
+  render() {
+    return (
+      <main>
+        App
+      </main>
+    )
+  };
 }
 
-export default App;
+export default connect()(App);
