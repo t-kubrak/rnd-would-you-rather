31b8515990a55f68b4e9eed2463e51c85ac93328
t-kubrak <chikashikaari@gmail.com>
2021-02-28 19:58:01 +0200

    refactor: set authed user id state

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,8 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 
-export function setAuthedUser (user) {
+export function setAuthedUser (userId) {
   return {
     type: SET_AUTHED_USER,
-    user,
+    userId,
   }
 }
