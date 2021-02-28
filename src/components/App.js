3f4d8a4fb0c037e4c531bfbd8d115e20a9661fcd
t-kubrak <chikashikaari@gmail.com>
2021-02-28 20:09:26 +0200

    feat: show sign in page; initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import {handleInitialData} from "../actions/shared";
 import Questions from "./Questions";
 import Header from "./Header";
 import QuestionPage from "./QuestionPage";
+import Signin from "./Signin";
 
 class App extends Component {
     componentDidMount() {
@@ -15,12 +16,18 @@ class App extends Component {
         return (
             <Router>
                 <Header/>
+                {/*TODO: prevent from accessing if not signed*/}
+                {this.props.authRequired ?
+                    <Redirect to='/sign-in' />
+                    : ''
+                }
                 <main>
                     {this.props.loading === true
                         ? null
                         : <div>
                             <Route path='/' exact component={Questions}/>
                             <Route path='/questions/:id' component={QuestionPage}/>
+                            <Route path='/sign-in' component={Signin}/>
                         </div>
                     }
                 </main>
@@ -31,7 +38,8 @@ class App extends Component {
 
 function mapStateToProps({authedUser}) {
     return {
-        loading: authedUser === null
+        loading: authedUser === null,
+        authRequired: !authedUser,
     }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from "react-redux";
 import { NavLink } from 'react-router-dom'
 
-export default function Header () {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to='/' exact activeClassName='active'>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/add' activeClassName='active'>New Question</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/leaderboard' activeClassName='active'>Leaderboard</NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
+class Header extends Component {
+    handleSignOut = () => {
+        // TODO
+    }
+
+    render() {
+        const {authedUser} = this.props;
+
+        return (
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to='/' exact activeClassName='active'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/add' activeClassName='active'>New Question</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/leaderboard' activeClassName='active'>Leaderboard</NavLink>
+                    </li>
+                </ul>
+                {authedUser
+                    ? <button onClick={this.handleSignOut}>Sign out</button>
+                    : <NavLink to='/sign-in'>Leaderboard</NavLink>}
+            </nav>
+        )
+    }
 }
+
+function mapStateToProps ({authedUser}) {
+    return {
+        authedUser
+    }
+}
+
+export default connect(mapStateToProps)(Header)
