d35093a3ab391a4755f45019f3257a0cf4b1e351
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-01 21:38:50 +0200

    feat: add answer stats

--- src/components/QuestionAnswered.js
+++ src/components/QuestionAnswered.js
@@ -6,34 +6,53 @@ class QuestionAnswered extends Component {
     render() {
         const { question, user, authedUserProfile } = this.props
 
-        if (question === null) {
+        if (!question) {
             return <p>This question doesn't exist</p>
         }
 
-        const { id, optionOne, optionTwo } = question
+        const { id, optionOne, optionTwo, votesCount } = question
 
         return (
             <li key={id}>
                 <img className='avatar' src={user.avatarURL}/>
                 <p>Asked by {user.name}</p>
-                <p>Results</p>
-                <p>{optionOne.text} {authedUserProfile.answers[id] === 'optionOne' ? '- Your choice'  : ''}</p>
-                <p>{optionTwo.text} {authedUserProfile.answers[id] === 'optionTwo' ? '- Your choice'  : ''}</p>
+                <p>Results:</p>
+                <ol>
+                    <li>
+                        {authedUserProfile.answers[id] === 'optionOne'
+                            ? <p className="bold">Would you rather {optionOne.text}? (Your choice)</p>
+                            : <p>Would you rather {optionOne.text}?</p>}
+                        <p>{optionOne.votes.length} out of {votesCount} votes</p>
+                        <p>{optionOne.percentage}%</p>
+                    </li>
+                    <li>
+                        {authedUserProfile.answers[id] === 'optionTwo'
+                            ? <p className="bold">Would you rather {optionTwo.text}? (Your choice)</p>
+                            : <p>Would you rather {optionTwo.text}?</p>}
+                        <p>{optionTwo.votes.length} out of {votesCount} votes</p>
+                        <p>{optionTwo.percentage}%</p>
+                    </li>
+                </ol>
+
+
             </li>
         )
     }
 }
 
 function mapStateToProps (state, {id}) {
-    const {authedUser, users, questions} = state;
+    const {users, questions} = state;
     const authedUserProfile = getAuthedUserProfile(state);
     const question = questions[id]
     const user = users[question.author]
+    question.votesCount = question.optionOne.votes.length + question.optionTwo.votes.length;
+    question.optionOne.percentage = Math.round(question.optionOne.votes.length / question.votesCount * 100)
+    question.optionTwo.percentage = Math.abs(100 - question.optionOne.percentage)
 
     return {
         authedUserProfile,
-        question: question || null,
-        user: user || null
+        question: question,
+        user: user
     }
 }
 
